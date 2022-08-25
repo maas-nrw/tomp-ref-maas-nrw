6cf5bd86184b09ec17a9ef4ba7076b355b85880a
Rainer <rainer.leopold@hilbigit.com>
2022-08-25 17:57:29 +0200

    Ersatz des nicht mehr im java enthaltenen xml-Pakets. mvn compile läuft in den einzelnen Teilprojekten, demo.bat noch nicht

--- tomp_meta/src/main/java/io/swagger/api/ApiResponseMessage.java
+++ tomp_meta/src/main/java/io/swagger/api/ApiResponseMessage.java
@@ -1,9 +1,9 @@
 package io.swagger.api;
 
-import javax.xml.bind.annotation.XmlTransient;
+import jakarta.xml.bind.annotation.XmlTransient;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-05-06T06:58:30.612Z[GMT]")
-@javax.xml.bind.annotation.XmlRootElement
+@jakarta.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
     public static final int ERROR = 1;
     public static final int WARNING = 2;
